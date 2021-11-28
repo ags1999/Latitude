a57e7b8ddc1950efc57147da5170ab8fae5fa9fe
Renan Araujo <75762102+RenanAraujo23@users.noreply.github.com>
2021-11-28 14:41:59 -0300

    Create ExploradorTest.java
    Refazer testes após inclusão de metas em locais pré-definidos

---
+++ model/ExploradorTest.java
@@ -0,0 +1,26 @@
+package model;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import model.*;
+import java.awt.Color;
+
+public class ExploradorTest {
+	Tabuleiro T = new Tabuleiro();
+	
+	@Test
+	public void criouExp()
+	{
+		Jogador J1 = new Jogador(Color.BLACK, 1, 0);
+		Explorador ex1 = new Explorador(0,J1);
+		assertNotNull("Criou novo explorador", ex1);
+	}
+	
+	@Test
+	public void verificaPos()
+	{
+		Jogador J1 = new Jogador(Color.BLACK, 1, 0);
+		Explorador ex1 = new Explorador(0,J1);
+		assertTrue(ex1.setMovimento(5, 7));
+	}
+}
